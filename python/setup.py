3eb7a3a33413ab645780dbde7cb3014ec324b5d8
Marine Chaput <marine.chaput@nerf.be>
2019-10-17 13:20:30 +0000

    automatize version process

--- python/setup.py
+++ python/setup.py
@@ -46,6 +46,18 @@ extensions = [
     )
 ]
 
+import re
+
+VERSIONFILE = "fklab/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+
 setup(
     name = "py-compressed-decoder",
     version = "0.2.0",
